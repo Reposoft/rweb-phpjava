e3840265d307d1509d493ae6bcb28d2194c13da9
Staffan Olsson <staffan@repos.se>
2014-02-22 14:08:26 +0000

    same name as in modified resin build, https://github.com/solsson/svn-caucho-com-resin

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cp repos/build.xml target/
 pushd target/
 ant encode.templates
 popd
-cp -r quercus-war-4.0/* target/repos-web/
+cp -r quercus-webapp/* target/repos-web/
 cp -r target/rweb-phpjava/WEB-INF/* target/repos-web/WEB-INF/
 echo "<?php phpinfo(); ?>" > target/repos-web/phpinfo.php
 
