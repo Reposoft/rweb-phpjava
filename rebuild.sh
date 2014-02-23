eb74e38ec85d4c53335725ed0a6484c199639cd1
Staffan Olsson <staffan@repos.se>
2014-02-23 23:00:28 +0000

    Temporary solution to getting compiled stuff from resin src

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 cp -r target/classes/* target/repos-web/WEB-INF/classes/
 cp -r src/main/webapp/WEB-INF/* target/repos-web/WEB-INF/
+# symlink?
+cp -r ../resin/modules/quercus/bin/* target/repos-web/WEB-INF/classes/
+
 touch target/repos-web/WEB-INF/web.xml
